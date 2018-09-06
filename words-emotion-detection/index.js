0b576129b4381a837faa5bc5dcb8091562923097
Muhammed Nuhman <nuhman7@outlook.com>
2018-09-06 11:05:54 +0530

    add testing code

---
+++ words-emotion-detection/index.js
@@ -0,0 +1,37 @@
+const brain = require("brain.js");
+
+// import our datasets
+const happy = require('./data-happy');
+const sad = require("./data-sad");
+
+const moods = [
+  ...happy,
+  ...sad
+];
+
+const serializer = require('./serialize_data');
+
+const net = new brain.NeuralNetwork()
+// start training
+//console.log(serializer.serialize(moods))
+
+net.train(serializer.serialize(moods), {log: true})
+
+// start predictions
+
+let test_strings = [
+  "I'll be good",
+  "I feel not bad",
+  "This is great",
+  "This is good",
+  "This is bad"
+]
+
+let maxLength = serializer.getMaxLength(moods);
+
+test_strings.map(str => {  
+  let sample = serializer.encode(str);
+  let data = serializer.makeLengthSame([{input: sample}], maxLength);
+  let pred_output = net.run(data[0].input);
+  console.log(str + ": " , pred_output);  
+});
